b2c1ca1288658af77766584fccb5914cddd7a338
zhan2819 <jasmine199910@gmail.com>
2021-02-13 20:56:55 -0800

    php sql connect

--- add_sub_activity.php
+++ add_sub_activity.php
@@ -6,18 +6,14 @@ $host = 'localhost';
 $port = 3307;
 
 $link = mysqli_init();
-$conn = mysqli_real_connect(
-   $link, 
-   $host, 
-   $user, 
-   $password, 
-   $db,
-   $port
-);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+$conn = $link -> real_connect($host, $user, $password, $db, $port);
+echo($conn);
+if ($result = $link -> query("SELECT * FROM ages")) {
+    echo "Returned rows are: " . $result -> num_rows;
+    // Free result set
+    $result -> free_result();
+  }
+$link -> close();
 
 ?>
     <div id="box">
@@ -28,11 +24,11 @@ if ($conn->connect_error) {
         </div>
         <div id="start_date_box" class = "start_date_box">
             <h3>Start Date:</h3>
-            <input type="date" id = "start_date_input_box" class = "start_date_input_box">
+            <input type="date" id = "start_date_input_box">
         </div>
         <div id="end_date_box" class = "end_date_box">
             <h3>End Date:</h3>
-            <input id ="end_date_input_box" class = "end_date_input_box" type="date">
+            <input id ="end_date_input_box" type="date">
         </div>
         <div id="duration_box">
             <h3 class = "duration">Duration:</h3>
@@ -40,9 +36,9 @@ if ($conn->connect_error) {
         </div>
         <div id="party_involved" class = "party_involved">
             <h3>Party Involved</h3>
-            <select id="party_involved_box">
+            <select>
                 <option value = "none">Choose an Option</option>
-                <option value="Carpet USA">Carpet USA</option>
+                <option value="Carpet_USA">Carpet USA</option>
                 <option value ="Helix">Helix</option>
                 <option value = "Southland">Southland</option>
             </select>
@@ -52,5 +48,5 @@ if ($conn->connect_error) {
             <div id="relationship_div"></div> 
             <button>Input Relationships</button>
         </div>
-        <button type="submit" onclick="form_to_schedule(this)">Submit Button</button>
+        <button type="submit" onclick="Add_Activity_title()">Submit Button</button>
     </div>

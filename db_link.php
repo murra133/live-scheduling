55e0407e04c3b0c9dbe5400edd1873bdb6cf361a
zhan2819 <jasmine199910@gmail.com>
2021-02-14 19:40:13 -0800

    hide warning

--- db_link.php
+++ db_link.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('display_errors', '0');
 $user = 'root';
 $password = '';
 $db = 'test_schedule';
@@ -6,7 +7,7 @@ $host = 'localhost';
 $port = 3306;
 $link = mysqli_init();
 $conn = $link -> real_connect($host, $user, $password, $db, $port);
-if ($conn == 1){
+if ($conn === 1){
     echo($conn);
     if ($result = $link -> query("SELECT * FROM ages")) {
         echo "Returned rows are: " . $result -> num_rows;
@@ -17,15 +18,16 @@ if ($conn == 1){
 }
 else{
     $password = 'root';
-$port = 3307;
-$conn = $link -> real_connect($host, $user, $password, $db, $port);
-echo($conn);
-if ($result = $link -> query("SELECT * FROM ages")) {
-    echo "Returned rows are: " . $result -> num_rows;
-    // Free result set
-    $result -> free_result();
-  }
-$link -> close();
+    $port = 3307;
+    $db = 'coursera';
+    $conn = $link -> real_connect($host, $user, $password, $db, $port);
+    echo($conn);
+    if ($result = $link -> query("SELECT * FROM ages")) {
+        echo "Returned rows are: " . $result -> num_rows;
+        // Free result set
+        $result -> free_result();
+    }
+    $link -> close();
 }
 
 ?>

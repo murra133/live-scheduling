9946008e293dbcd70c3cb108ed56ac5e804c78ad
zhan2819 <jasmine199910@gmail.com>
2021-02-14 23:52:17 -0800

    clean code

--- PHP/db_link.php
+++ PHP/db_link.php
@@ -8,7 +8,6 @@ $port = 3306;
 $link = mysqli_init();
 $conn = $link -> real_connect($host, $user, $password, $db, $port);
 if ($conn === 1){
-    echo($conn);
     if ($result = $link -> query("SELECT * FROM ages")) {
         echo "Returned rows are: " . $result -> num_rows;
         // Free result set
@@ -19,7 +18,6 @@ else{
     $password = 'root';
 $port = 3307;
 $conn = $link -> real_connect($host, $user, $password, $db, $port);
-echo($conn);
 if ($result = $link -> query("SELECT * FROM ages")) {
     echo "Returned rows are: " . $result -> num_rows;
     // Free result set

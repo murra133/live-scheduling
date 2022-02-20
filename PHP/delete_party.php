6ecd6a40d1b39b7b9078044d16f03b5dd2ebb766
zhan2819 <jasmine199910@gmail.com>
2022-02-19 19:30:01 -0800

    some party change

--- PHP/delete_party.php
+++ PHP/delete_party.php
@@ -3,7 +3,7 @@ require_once('db_link.php');
 $project_id=$_POST['project_id'];
 $id = $_POST['id'];
 
-    $stmt = $link->prepare("DELETE FROM parties".$project_id." WHERE PARTY_ID = ?");
+    $stmt = $link->prepare("DELETE FROM parties_".$project_id." WHERE PARTY_ID = ?");
     $stmt->bind_param("i",$id);
     $stmt->execute();
 
